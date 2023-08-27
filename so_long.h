004d42b724d054911eb2f60d1deceb8455e6648f
hnagasak <hnagasak@student.42tokyo.jp>
2023-08-28 08:54:06 +0900

    create so_long.h

---
+++ so_long.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 07:59:50 by hnagasak          #+#    #+#             */
+/*   Updated: 2023/08/28 08:49:12 by hnagasak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+// # include "./libft/libft.h"
+# include "./minilibx-linux/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# define KEY_ESC 65307
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_UP 65362
+# define KEY_LEFT 65361
+# define KEY_DOWN 65364
+# define KEY_RIGHT 65363
+
+# define BUFFER_SIZE 42
+# define TILE_SIZE 32
+
+#endif
