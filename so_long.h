485fe934d7ef078939cd955d35e339957c3904df
hnagasak <hnagasak@student.42tokyo.jp>
2023-09-08 17:53:22 +0900

    update header file

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 07:59:50 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/08/28 08:49:12 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/09/03 03:34:51 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,47 @@
 # define KEY_DOWN 65364
 # define KEY_RIGHT 65363
 
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 1000
 # define TILE_SIZE 32
+
+typedef struct s_position
+{
+	int			x;
+	int			y;
+	int			direction;
+}				t_position;
+
+typedef struct s_images
+{
+	void		*img_wall;
+	void		*img_space[10];
+	void		*img_item;
+	void		*img_player;
+	void		*img_exit;
+}				t_images;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*window;
+	int			width;
+	int			height;
+	t_images *images;
+	// void		*img_wall;
+	// void		*img_space;
+	// void		*img_item;
+	// void		*img_player;
+	// void		*img_exit;
+	t_position	*player;
+	t_position	*exit;
+	char		*map;
+	int			item_count;
+}				t_game;
+
+void	move_player(t_game *game, int keycode);
+
+// for debug
+void	show_stat(t_game *g, int num);
+void	test_render_map(t_game *g, int i, int j);
 
 #endif
